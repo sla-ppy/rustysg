f5a639f2be19caff4fe9a1090d1dc1673a5b915d
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-08 00:26:59 +0100

    add basic String replacement

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Debug, Clone, clap::Subcommand)]
 enum Command {
     Create { path: PathBuf },
-    Generate,
+    Generate { path: PathBuf },
 }
 
 #[derive(Debug, clap::Parser)]
@@ -20,8 +20,8 @@ fn main() -> anyhow::Result<()> {
         Command::Create { path } => {
             create(path)?;
         },
-        Command::Generate => {
-            generate()?;
+        Command::Generate { path } => {
+            generate(path)?;
         },
     }
     Ok(())
@@ -30,10 +30,18 @@ fn main() -> anyhow::Result<()> {
 fn create(path: PathBuf) -> anyhow::Result<()> {
     fs::create_dir_all(&path)?;
     fs::write(&format!("{}/_config.toml", path.to_string_lossy()), "# todo!")?;
+    fs::create_dir(&format!("{}/template", path.to_string_lossy()))?;
+    fs::write(&format!("{}/template/base_template.html", path.to_string_lossy()), "")?;
+    fs::write(&format!("{}/content.html", path.to_string_lossy()),"")?;
     Ok(())
 }
 
-fn generate() -> anyhow::Result<()> {
+fn generate(path: PathBuf) -> anyhow::Result<()> {
+    let path: String = path.to_string_lossy().to_string();
+    let mut template: String = fs::read_to_string(path + "/template/base_template.html")?;
     println!("Generating!");
+    let input = "Hello World!";
+    template = template.replace("{{ content.html }}", input);
+    println!("{}", template);
     Ok(())
 }
