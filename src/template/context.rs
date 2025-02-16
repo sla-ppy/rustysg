6eb057bc60277bc11a994127dd9b2edfe36798b1
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-16 16:36:00 +0100

    add parsing from yaml header

--- src/template/context.rs
+++ src/template/context.rs
@@ -1,7 +1,7 @@
 use chrono::{NaiveDate, NaiveTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
 pub struct Metadata {
     #[serde(default)]
     pub title: String,
@@ -18,7 +18,10 @@ pub struct Metadata {
 impl Metadata {
     /// Create a new Metadata instance from a YAML string,
     /// replacing missing fields with default values from `default_metadata`.
-    pub fn from_yaml(yaml: &str, default_metadata: Option<Metadata>) -> Result<Self, serde_yml::Error> {
+    pub fn from_yaml(
+        yaml: &str,
+        default_metadata: Option<Metadata>,
+    ) -> Result<Self, serde_yml::Error> {
         let mut meta: Metadata = serde_yml::from_str(yaml)?;
         if let Some(default) = default_metadata {
             if meta.title.is_empty() {
@@ -47,34 +50,46 @@ impl Context {
     }
 
     /// Create a new Context instance from a file on disk, using the file's metadata
-    /// where possible to substitute missing fields in the metadata.
-    pub fn from_file(filename: &str) -> Result<Self, std::io::Error> {
-        let content = std::fs::read_to_string(filename)?;
+    /// where possible to substitute missing fields in the metadata. The metadata is
+    /// then filled out from the yaml header in the file (denoted by `---`), if present.
+    pub fn from_file(filename: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut content = std::fs::read_to_string(filename)?;
         // for the date and time of publish/writing, we want to default to the
         // date and time of the file itself on disk. If you create a new file on
         // 1st August 2021 at 12:00, the date and time of the file will be used
         // as the date and time of publish/writing, so that the article can say
         // "published on 1st August 2021 at 12:00".
-        let file_metadata = std::fs::metadata(filename)?;
-        let file_created_systime = file_metadata.created().unwrap();
+        let file_created_systime = std::fs::metadata(filename)?.created().unwrap();
         let file_created_datetime = chrono::DateTime::<Utc>::from(file_created_systime);
-        let metadata = Metadata::from_yaml(&content, Some(Metadata {
+        let file_metadata = Metadata {
             date: file_created_datetime.date_naive(),
             time: file_created_datetime.time(),
             ..Default::default()
-        }))?;
+        };
+        let mut metadata = file_metadata.clone();
+        // the content may start with a yaml header, which is denoted by `---`.
+        // In this case, we want to parse the yaml header and use it to override
+        // the "default" metadata fields
+        if content.starts_with("---") {
+            content = content[3..].to_string();
+            let end = content.find("---");
+            if let Some(end) = end {
+                let yaml_header = &content[..end];
+                metadata = Metadata::from_yaml(&yaml_header, Some(file_metadata))?;
+                content = content[end + 3..].to_string();
+            }
+        }
         Ok(Self::new(content, metadata))
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Context {
     pub content: String,
     pub metadata: Metadata,
 }
 
-impl Metadata {
-}
+impl Metadata {}
 
 #[cfg(test)]
 mod context_tests {
@@ -82,13 +97,31 @@ mod context_tests {
 
     #[test]
     fn test_file_metadata_context() {
-        let filename = "Cargo.toml";
+        let filename = "tests/yaml-test.html";
         let ctx = Context::from_file(filename).unwrap();
-        assert_eq!("Hello, World!", ctx.metadata.title);
-        assert_eq!("This is a test.", ctx.metadata.description);
-        assert_eq!("Lion", ctx.metadata.author);
-        assert_eq!(NaiveDate::from_ymd_opt(2021, 8, 1).unwrap(), ctx.metadata.date);
-        assert_eq!(NaiveTime::from_hms_opt(12, 0, 0).unwrap(), ctx.metadata.time);
+        assert_eq!("My Post", ctx.metadata.title);
+        assert_eq!("This is my first post", ctx.metadata.description);
+        assert_eq!("John Doe", ctx.metadata.author);
+        assert_eq!(
+            NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+            ctx.metadata.date
+        );
+        assert_eq!(
+            NaiveTime::from_hms_opt(12, 30, 0).unwrap(),
+            ctx.metadata.time
+        );
+    }
+
+    #[test]
+    fn test_file_metadata_from_created_date() {
+        let filename = "tests/yaml-test-nodate.html";
+        let file_datetime = {
+            let file_created_systime = std::fs::metadata(filename).unwrap().created().unwrap();
+            chrono::DateTime::<Utc>::from(file_created_systime)
+        };
+        let ctx = Context::from_file(filename).unwrap();
+        assert_eq!(file_datetime.date_naive(), ctx.metadata.date);
+        assert_eq!(file_datetime.time(), ctx.metadata.time);
     }
 }
 
